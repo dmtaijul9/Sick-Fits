614db4221de9f51c7b74c911fc18d14bd81139e5
Taijul Islam <taijulislam.st9@gmail.com>
2021-12-23 15:02:17 +0600

    feat(class 10-14): keystone basic

---
+++ backend/keystone.ts
@@ -0,0 +1,56 @@
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
+import {
+  statelessSessions,
+  withItemData,
+} from "@keystone-next/keystone/session";
+import { User } from "./schemas/User";
+import "dotenv/config";
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+
+    //TODO: Add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    //@ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      //TODO: add data seeding here
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      //show the ui only for people who pass this test .
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: "id email name",
+    }),
+  })
+);
